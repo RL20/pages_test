77d83c498e50d17200e1e3aa4484cdb43e0a1c56
RL20 <harel_shalom@yahoo.com>
2022-08-29 03:03:51 +0300

    first commit

--- .history/src/App_20220829025836.js
+++ .history/src/App_20220829025836.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Learn How to Use github Pages
+        <p>Edit and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
       </header>
     </div>
   );
